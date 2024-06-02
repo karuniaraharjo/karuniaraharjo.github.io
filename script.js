b5a2a5a294226a25db349ebb3f4e8f91429af17a
Runsz <karuniaraharjo06@gmail.com>
2024-06-02 10:08:03 +0700

    rev 1

--- script.js
+++ script.js
@@ -2,24 +2,22 @@ window.addEventListener('scroll', function() {
     var header = document.querySelector('.header');
     var scrollPosition = window.scrollY;
 
-    if (scrollPosition > 100) { // Jika scroll lebih dari 100px dari atas
-        header.style.backgroundColor = 'rgba(18, 18, 18, 0.4)'; // Atur latar belakang navigasi menjadi lebih transparan
+    if (scrollPosition > 100) {
+        header.style.backgroundColor = 'rgba(18, 18, 18, 0.4)';
     } else {
-        header.style.backgroundColor = 'rgba(18, 18, 18, 0.8)'; // Kembalikan latar belakang navigasi ke transparansi default
+        header.style.backgroundColor = 'rgba(18, 18, 18, 0.8)';
     }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const navToggle = document.querySelector('.nav-toggle');
-    const nav = document.querySelector('.nav ul');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const navToggle = document.querySelector('.nav-toggle');
+//     const nav = document.querySelector('.nav ul');
 
-    window.addEventListener('resize', function() {
-        if (window.innerWidth > 768) {
-            nav.style.display = 'flex';
-        } else if (!navToggle.checked) {
-            nav.style.display = 'none';
-        }
-    });
-});
-
-AOS.init()
+//     window.addEventListener('resize', function() {
+//         if (window.innerWidth > 768) {
+//             nav.style.display = 'flex';
+//         } else if (!navToggle.checked) {
+//             nav.style.display = 'none';
+//         }
+//     });
+// });
